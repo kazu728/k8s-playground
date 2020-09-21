ad95590c5ae0818e195553eeefb843f820df278d
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-09-21 13:38:42 +0900

    chore:gce

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
 FROM node:12.18.0-alpine
+LABEL maintainer="kazuki728"
 
 WORKDIR /app
+
+RUN apk update && \
+    apk upgrade && \
+    apk add tzdata
+
+RUN cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime && \
+    echo "Asia/Tokyo" > /etc/timezone && \
+    apk del tzdata 
 
 ADD ./app .
 CMD [ "yarn", "start" ]
--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,11 @@
 const http = require("http");
 const server = http.createServer();
 
-server.on("request", async (req, res) => {
+server.on("request", async (_, res) => {
   await res.writeHead(200, { "Content-Type": "text/plain" });
 
   await res.write("Hello world!");
   res.end();
 });
-
-console.log("[server] listen at port:5000");
-server.listen(process.env.PORT || 5000);
-
+console.log("Listen on port:5000");
+server.listen(5000) 
